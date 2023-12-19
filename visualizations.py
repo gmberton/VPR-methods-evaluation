409df4f4753f6fa176c71595c63a1d8dfe33a234
Gabriele Berton <49533159+gmberton@users.noreply.github.com>
2023-12-19 15:53:45 +0100

    Update visualizations.py for grayscale images

--- visualizations.py
+++ visualizations.py
@@ -41,7 +41,7 @@ def build_prediction_image(images_paths, preds_correct=None):
     assert len(images_paths) == len(preds_correct)
     labels = ["Query"] + [f"Pr{i} - {is_correct}" for i, is_correct in enumerate(preds_correct[1:])]
     num_images = len(images_paths)
-    images = [np.array(Image.open(path)) for path in images_paths]
+    images = [np.array(Image.open(path).convert("RGB")) for path in images_paths]
     for img, correct in zip(images, preds_correct):
         if correct is None:
             continue
