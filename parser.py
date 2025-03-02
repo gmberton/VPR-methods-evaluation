ac83164eab9fadd5ef32b8722743dc390709357e
Gabriele Berton <s252805@studenti.polito.it>
2025-03-02 08:52:41 +0100

    Set best performing dimensionality for each method

--- parser.py
+++ parser.py
@@ -113,7 +113,7 @@ def parse_arguments():
         if args.backbone is None:
             args.backbone = "ResNet50"
         if args.descriptors_dimension is None:
-            args.descriptors_dimension = 512
+            args.descriptors_dimension = 2048
         if args.backbone == "VGG16" and args.descriptors_dimension not in [64, 128, 256, 512]:
             raise ValueError("When using CosPlace with VGG16 the descriptors_dimension must be in [64, 128, 256, 512]")
         if args.backbone == "ResNet18" and args.descriptors_dimension not in [32, 64, 128, 256, 512]:
@@ -137,7 +137,7 @@ def parse_arguments():
         if args.backbone is None:
             args.backbone = "ResNet50"
         if args.descriptors_dimension is None:
-            args.descriptors_dimension = 512
+            args.descriptors_dimension = 8192
         if args.backbone not in [None, "ResNet50"]:
             raise ValueError("When using Conv-AP the backbone must be None or ResNet50")
         if args.descriptors_dimension not in [None, 512, 2048, 4096, 8192]:
@@ -149,7 +149,7 @@ def parse_arguments():
         if args.backbone is None:
             args.backbone = "ResNet50"
         if args.descriptors_dimension is None:
-            args.descriptors_dimension = 512
+            args.descriptors_dimension = 4096
         if args.backbone not in [None, "ResNet50"]:
             raise ValueError("When using Conv-AP the backbone must be None or ResNet50")
         if args.descriptors_dimension not in [None, 128, 512, 4096]:
@@ -159,7 +159,7 @@ def parse_arguments():
         if args.backbone is None:
             args.backbone = "ResNet50"
         if args.descriptors_dimension is None:
-            args.descriptors_dimension = 512
+            args.descriptors_dimension = 2048
         if args.backbone == "VGG16" and args.descriptors_dimension not in [512]:
             raise ValueError("When using EigenPlaces with VGG16 the descriptors_dimension must be in [512]")
         if args.backbone == "ResNet18" and args.descriptors_dimension not in [256, 512]:
