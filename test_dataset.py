7a564364ad5001438670971edd786ddc272a211d
gmberton <s252805@studenti.polito.it>
2025-03-14 15:45:05 +0100

    takes also PNG images besides jpg

--- test_dataset.py
+++ test_dataset.py
@@ -12,15 +12,15 @@ def read_images_paths(dataset_folder):
     """Find images within 'dataset_folder'. If the file
     'dataset_folder'_images_paths.txt exists, read paths from such file.
     Otherwise, use glob(). Keeping the paths in the file speeds up computation,
-    because using glob over large folders might be slow.
+    because using glob over very large folders might be slow.
 
     Parameters
     ----------
-    dataset_folder : str, folder containing JPEG images
+    dataset_folder : str, folder containing images
 
     Returns
     -------
-    images_paths : list[str], paths of JPEG images within dataset_folder
+    images_paths : list[str], paths of images within dataset_folder
     """
 
     if not os.path.exists(dataset_folder):
@@ -41,9 +41,10 @@ def read_images_paths(dataset_folder):
             )
     else:
         print(f"Searching test images in {dataset_folder} with glob()")
-        images_paths = sorted(glob(f"{dataset_folder}/**/*.jpg", recursive=True))
+        images_paths = sorted(glob(f"{dataset_folder}/**/*", recursive=True))
+        images_paths = [p for p in images_paths if os.path.isfile(p) and os.path.splitext(p)[1].lower() in [".jpg", ".jpeg", ".png"]]
         if len(images_paths) == 0:
-            raise FileNotFoundError(f"Directory {dataset_folder} does not contain any JPEG images")
+            raise FileNotFoundError(f"Directory {dataset_folder} does not contain any images")
     return images_paths
 
 
